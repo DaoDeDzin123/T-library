115f291b64fb5bd98be1c46ca71cefe189a2329b
SoullessCookie <nikaabzyanova@gmail.com>
2026-03-15 19:51:43 +0700

    feat: create structure

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+def safe_books():
+    pass
 
+def upload_books():
+    pass
+
+def get_books():
+    pass
+
+def add_book():
+    pass
+
+def update_book():
+    pass
+
+def delete_book():
+    pass
+
+def search_book():
+    pass
+
+def greeting():
+    pass
+
+if __name__ == "__main__":
+    try:
+        upload_books()
+        greeting()
+        while True:
+            input()
+    except:
+        print("Ok")
+    finally:
+        safe_books()
